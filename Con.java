0f1328b67283f6263579ca66eb747ffefefd769f
HIBA KT <hibagupt@gmail.com>
2025-03-21 15:26:31 +0530

    Add files via upload

---
+++ Con.java
@@ -0,0 +1,15 @@
+public class Con
+{
+public static void main(String[]args)
+{
+String fname="john";
+String sname="den";
+System.out.println(fname.concat(sname));
+}
+}
+
+         
+
+            
+
+            
